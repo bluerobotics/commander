c463831b256faefbffda11cda51e6f208c418102
dheideman <dheideman@socal.rr.com>
2017-10-10 10:29:40 -0700

    Refactor code

---
+++ Thruster-Commander/Blinker.h
@@ -0,0 +1,56 @@
+/* Blue Robotics Thruster Commander Firmware
+-----------------------------------------------------
+
+Title: Blue Robotics Thruster Commander Firmware - Blinker
+
+Description: This code is the default firmware for the Blue Robotics
+Thruster Commander, which provides a simple interface to control a
+bidirectional speed controller with PWM signals. It can be used to test
+motors or to control simple vehicles like a kayak.
+
+The code is designed for the ATtiny44 microcontroller and can be compiled and
+uploaded via the Arduino 1.0+ software.
+
+-------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2017 Blue Robotics Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-------------------------------*/
+
+#ifndef BLINKER
+#define BLINKER
+
+#include "Thruster-Commander.h"
+
+// Struct Type Definition
+struct blinker_t {
+  unsigned long lastblink;  // timestamp (ms)
+  unsigned int  period;     // ms
+  int           pin;
+  bool          state;
+};
+
+// Function Declaration
+int  pwmToBlinkPeriod(int pulsewidth);
+void initializeLEDs(blinker_t leds[]);
+void setLEDs(blinker_t leds[], int pwmOut0, int pwmOut1);
+
+#endif
