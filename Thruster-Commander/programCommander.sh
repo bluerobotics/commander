0a134017551970993678b5d21f2fc8c1b679ee01
dheideman <dheideman@socal.rr.com>
2017-12-27 18:06:46 -0800

    Fix programming script settings

--- Thruster-Commander/programCommander.sh
+++ Thruster-Commander/programCommander.sh
@@ -12,31 +12,22 @@ NC='\033[0m'
 
 
 # Configuration
-avrdude="/home/daniel/Applications/arduino-1.8.5/hardware/tools/avr/bin/avrdude"
+avrdude="avrdude"
 programmer="usbtiny"
 conffile="/etc/avrdude.conf"
-#conffile="/home/daniel/Applications/arduino-1.8.5/hardware/tools/avr/etc/avrdude.conf"
 microcontroller="t84"
-hexfile="/home/daniel/dev/Commander/commander/Thruster-Commander/Thruster-Commander.ino.tiny14.hex"
+hexfile="Thruster-Commander.ino.tiny14.hex"
 
 # Flash Thruster Commander
 echo "Starting Thruster Commander Flash Sequence..."
 
-# Write firmware
-#echo "Writing Firmware..."
-#$avrdude -C $conffile -c $programmer -p $microcontroller -U flash:w:$hexfile 2>&1 | tee firmware.log
-
-# Set fuses
-#echo "Setting Fuses..."
-#$avrdude -C $conffile -c $programmer -p $microcontroller -e -Uefuse:w:0xff:m -Uhfuse:w:0xdf:m -Ulfuse:w:0xfe:m 2>&1 | tee fuses.log
-
 #Write program and set fuses
-$avrdude -C$conffile -c$programmer -p$microcontroller -e -Uefuse:w:0xff:m -Uhfuse:w:0xdf:m -Ulfuse:w:0xfe:m -Uflash:w:$hexfile 2>&1 | tee programcommander.log
+$avrdude -c$programmer -p$microcontroller -e -Uefuse:w:0xff:m -Uhfuse:w:0xdf:m -Ulfuse:w:0xfe:m -Uflash:w:$hexfile 2>&1 | tee programcommander.log
 
 echo -e "${BLUE}=========RESULT=========="
 echo -e "${GREEN}"
 grep "bytes of flash verified" programcommander.log
-grep "avrdude: safemode: Fuses OK (E:FF, H:DF, L:FE)" programcommander.log
+grep "avrdude: safemode: Fuses OK" programcommander.log
 echo -e "${RED}"
 grep "mismatch" programcommander.log
 echo -e "${NC}"
